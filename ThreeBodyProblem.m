06ff3c433742a1857999bc543fff10e3c567b452
Mike Croucher <mcrouche@mathworks.com>
2025-01-15 15:27:35 +0000

    Updated animation code to speed up and lower memory consumption

--- ThreeBodyProblem.m
+++ ThreeBodyProblem.m
@@ -19,27 +19,13 @@ x10 = P(1, 1); y10 = P(2, 1);
 x20 = P(1, 2); y20 = P(2, 2);
 x30 = P(1, 3); y30 = P(2, 3);
 
-% Set up figure for plotting
-figure;
-axis([-100 60 -100 60]); % Adjust axis limits as needed
-hold on;
-grid on; % Add a grid for better visualization
-title('Simulation Graphic');
-xlabel('x');
-ylabel('y');
-
 time = 0:dt:100;
 
 % Preallocate arrays for trajectory plotting
 x1 = 0*time; y1 = x1;
 x2 = x1;     y2 = x1;
 x3 = x1;     y3 = x1;
-
-% Initialize plot handles for bodies
-plotHandle1 = plot(P(1,1), P(2,1), 'ro', 'MarkerFaceColor', 'r'); % Body 1
-plotHandle2 = plot(P(1,2), P(2,2), 'go', 'MarkerFaceColor', 'g'); % Body 2
-plotHandle3 = plot(P(1,3), P(2,3), 'bo', 'MarkerFaceColor', 'b'); % Body 3
-
+fprintf("Computing\n");
 tic
 for i = 1:1:length(time)
 
@@ -87,39 +73,45 @@ for i = 1:1:length(time)
     x3(i) = P(1,3);
     y3(i) = P(2,3);
 
-%    % Update the positions of the bodies in the plot
-%    set(plotHandle1, 'XData', P(1,1), 'YData', P(2,1));
-%    set(plotHandle2, 'XData', P(1,2), 'YData', P(2,2));
-%    set(plotHandle3, 'XData', P(1,3), 'YData', P(2,3));
-
-    % Plot the new positions of the bodies and their trajectories
-%    plot(x1(1:t), y1(1:t), 'r', 'LineWidth', 1);
-%    plot(x2(1:t), y2(1:t), 'g', 'LineWidth', 1);
-%    plot(x3(1:t), y3(1:t), 'b', 'LineWidth', 1);
-
-%    drawnow 
 end
-
 toc
 
-%%plot
+fprintf("Making Animation\n");
+tic
+
+% Plot the objects
+h1 = plot(x1(1), y1(1), 'ro', 'MarkerFaceColor', 'r'); 
+axis([-100 60 -100 60]); % Adjust axis limits as needed
+grid on; % Add a grid for better visualization;
+xlabel('x');
+ylabel('y');
+
+hold on
+h2 = plot(x2(1), y2(1), 'go', 'MarkerFaceColor', 'g');
+h3 = plot(x3(1), y3(1), 'bo', 'MarkerFaceColor', 'b');
+% Plot the trails
+t1 = plot(NaN, NaN, 'r-'); % plot the trail
+t2 = plot(NaN, NaN, 'g-'); % plot the trail
+t3 = plot(NaN, NaN, 'b-'); % plot the trail
+
+
+%% Plot the trails
 
 dtplot = 0.1;
-
 for i=1:floor(dtplot/dt):length(time)
     % Update the positions of the bodies in the plot
-   set(plotHandle1, 'XData', x1(i), 'YData', y1(i));
-    set(plotHandle2, 'XData', x2(i), 'YData', y2(i));
-    set(plotHandle3, 'XData', x3(i), 'YData', y3(i));
-
-    % Plot the new positions of the bodies and their trajectories
-    plot(x1(1:i), y1(1:i), 'r');
-    plot(x2(1:i), y2(1:i), 'g');
-    plot(x3(1:i), y3(1:i), 'b');
-     
+    set(h1, 'XData', x1(i), 'YData', y1(i))
+    set(h2, 'XData', x2(i), 'YData', y2(i))
+    set(h3, 'XData', x3(i), 'YData', y3(i))
+    % Update the trails
+    set(t1, 'XData', x1(1:i), 'YData', y1(1:i))
+    set(t2, 'XData', x2(1:i), 'YData', y2(1:i))
+    set(t3, 'XData', x3(1:i), 'YData', y3(1:i))
     title(['Simulation Graphic at t =',num2str(time(i)) ]);
-    pause(eps*10^10)
+    drawnow
     if (max([abs(x1(i)),abs(x2(i)),abs(x1(i))])>10^5)
         break
     end
 end
+hold off
+toc
